876d11f79735ab0f19d6c2ca3f9623982b80287f
fengy <whyufeng@gmail.com>
2022-11-27 15:13:26 -0600

    init package

---
+++ public/src/books.js
@@ -0,0 +1,15 @@
+
+function findAuthorById(authors, id) {}
+
+function findBookById(books, id) {}
+
+function partitionBooksByBorrowedStatus(books) {}
+
+function getBorrowersForBook(book, accounts) {}
+
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
